b31ad5e8e6796e23b0b09fff3efcfb99adae2adc
StiversBrunus <bruno.oliveira_araujo@outlook.com>
2024-05-30 13:55:39 -0300

    order-api-development: Alter status response API
    Alter status response 200 to 201.

--- src/main/java/br/com/redventures/ramengo/api/order/request/OrderRequestController.java
+++ src/main/java/br/com/redventures/ramengo/api/order/request/OrderRequestController.java
@@ -3,6 +3,8 @@ package br.com.redventures.ramengo.api.order.request;
 import br.com.redventures.ramengo.api.BaseController;
 import br.com.redventures.ramengo.api.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -15,7 +17,7 @@ public class OrderRequestController extends BaseController {
     private OrderRequestApplicationService orderRequestApplicationService;
 
     @PostMapping
-    public OrderRequest open (@RequestHeader(name = "x-api-key") String tokenAuthentication,  @RequestBody OrderRequestForm orderRequestForm) throws ValidationException, IOException, InterruptedException {
+    public ResponseEntity<OrderRequest> open (@RequestHeader(name = "x-api-key") String tokenAuthentication, @RequestBody OrderRequestForm orderRequestForm) throws ValidationException, IOException, InterruptedException {
 
         // I use the form information
         // Injection token of the header on form with another information
@@ -25,6 +27,8 @@ public class OrderRequestController extends BaseController {
         BaseController baseController = new BaseController();
         baseController.auth(form);
 
-        return orderRequestApplicationService.order(form);
+        OrderRequest order = orderRequestApplicationService.order(form);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(order) ;
     }
 }
