fcd69e64b755ca540a082f997653220f9238cf19
StiversBrunus <bruno.oliveira_araujo@outlook.com>
2024-05-30 05:47:04 -0300

    order-api-development: put new static string for future implementations
    I put some static variable for use on future implementations.

--- src/main/java/br/com/redventures/ramengo/api/infra/Security.java
+++ src/main/java/br/com/redventures/ramengo/api/infra/Security.java
@@ -3,4 +3,15 @@ package br.com.redventures.ramengo.api.infra;
 public class Security {
 
     public static final String AUTH_TOKEN = "ZtVdh8XQ2U8pWI2gmZ7f796Vh8GllXoN7mr0djNf";
+
+    public static final String EXTERNAL_URL = "https://api.tech.redventures.com.br/orders/generate-id";
+
+    public static final String REQUEST_HEADER_NAME = "x-api-key";
+
+    public static final String KEY_JSON_NAME = "orderId";
+
+    public static final String RAMEN_EXTERNAL_IMAGE_URL = "https://tech.redventures.com.br/icons/ramen/";
+
+
+
 }
